900846db2b9164a19b5c5e4ea5f2e9f94328bcac
pedrovmcastro <pvmcastro@gmail.com>
2024-08-18 15:48:17 -0300

    update been_read VIEW

--- schema.sql
+++ schema.sql
@@ -129,9 +129,17 @@ SELECT * FROM "available_books" WHERE "location" = 'kindle';
 
 -- To view all books that have already been read
 CREATE VIEW "been_read" AS
-SELECT * FROM "available_books" 
-JOIN "books" ON "books"."id" = "available_books"."id"
-WHERE "is_read" = TRUE;
+SELECT 
+    "id", "title", "year",
+    (SELECT "first_name" || ' ' || "last_name"
+    FROM "authors"
+    JOIN "authored" ON "authored"."author_id" = "authors"."id"
+    WHERE "authored"."book_id" = "books"."id"
+    ORDER BY "authors"."last_name" LIMIT 1) AS "author",
+    "language", "rating", "location", "category", "genre"       
+FROM "books"
+WHERE "is_read" = TRUE
+ORDER BY "location", "author", "year";
 
 -- To view all books that have been sold (soft deletion)
 CREATE VIEW "sold_books" AS
@@ -163,7 +171,7 @@ SELECT
 FROM "books"
 JOIN "books_on_loan" ON "books_on_loan"."book_id" = "books"."id"
 JOIN "loans" ON "loans"."id" = "books_on_loan"."loan_id"
-ORDER BY "due_date", "lend_date";
+ORDER BY "due_date", "loan_date";
 
 -- To view all books that have been lent
 CREATE VIEW "lent_books" AS
